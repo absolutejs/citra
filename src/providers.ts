abf17ebbc796eddd075c5dea82f9c4fdadaeee39
Alex Kahn <alexkahndev@gmail.com>
2025-04-18 05:28:16 -0400

    Add provider type definitions and configurations for OAuth2 providers

--- src/providers.ts
+++ src/providers.ts
@@ -1,3 +1,14 @@
+type Provider = {
+	isPKCE: boolean;
+	isOIDC: boolean;
+	authorizationUrl: string;
+	tokenUrl: string;
+	tokenRevocationUrl?: string;
+	authorizationUrlParams?: Record<string, string>;
+	tokenUrlParams?: Record<string, string>;
+	tokenRevocationUrlParams?: Record<string, string>;
+}
+
 const providers = {
 	Google: {
 		isPKCE: true,
@@ -11,6 +22,12 @@ const providers = {
 		isOIDC: false,
 		authorizationUrl: 'https://www.facebook.com/v16.0/dialog/oauth',
 		tokenUrl: 'https://graph.facebook.com/v16.0/oauth/access_token'
+	},
+	Apple: {
+		isPKCE: true,
+		isOIDC: true,
+		authorizationUrl: 'https://appleid.apple.com/auth/authorize',
+		tokenUrl: 'https://appleid.apple.com/auth/token',
 	}
 };
 
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,23 @@ export type OAuth2Tokens = {
 	scope?: string;
 	id_token?: string;
 };
+
+export type ProviderOAuth2Config = {
+	clientId: string;
+	clientSecret: string;
+}
+
+export type GoogleOAuth2Config = ProviderOAuth2Config & {
+	redirectUri: string;
+}
+
+export type FacebookOAuth2Config = ProviderOAuth2Config & {
+	redirectUri: string;
+}
+
+export type AppleOAuth2Config = ProviderOAuth2Config & {
+	redirectUri: string;
+	teamId: string;
+	keyId: string;
+	pkcs8PrivateKey: Uint8Array;
+}

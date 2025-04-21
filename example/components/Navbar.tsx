d2c60ae2efc99844a42deabbb7be56de7d2882e3
Alex Kahn <alexkahndev@gmail.com>
2025-04-21 15:46:44 -0400

    refactor: Simplify Navbar component by removing unused navigation links

--- example/components/Navbar.tsx
+++ example/components/Navbar.tsx
@@ -8,12 +8,6 @@ type NavbarProps = {
 	setModalOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-const navLinks = [
-	{ href: '/page1', label: 'Page 1' },
-	{ href: '/page2', label: 'Page 2' },
-	{ href: '/protected', label: 'Protected' }
-];
-
 export const Navbar = ({
 	modalOpen,
 	setModalOpen
@@ -39,29 +33,9 @@ export const Navbar = ({
 				}}
 				href="/"
 			>
-				Absolute Auth
+				Citra
 			</a>
 
-			<nav style={{ display: 'flex', alignItems: 'center' }}>
-				{navLinks.map(({ href, label }, index) => (
-					<a
-						key={index}
-						href={href}
-						style={{
-							textDecoration: 'none',
-							fontSize: '1rem',
-							textWrap: 'nowrap',
-							fontWeight: 'bold',
-							marginRight: '20px',
-							padding: '5px 10px',
-							borderRadius: '5px',
-							color: '#fff'
-						}}
-					>
-						{label}
-					</a>
-				))}
-				
 				{modalOpen && (
 					<Modal
 						isOpen={modalOpen}
@@ -70,7 +44,6 @@ export const Navbar = ({
 						<AuthOptions />
 					</Modal>
 				)}
-			</nav>
 		</header>
 	);
 };

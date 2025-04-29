d4b1101c79350163a1747bce4cedf8849d0ae47a
Alex Kahn <alexkahndev@gmail.com>
2025-04-28 23:16:43 -0400

    feat: add Apple OAuth2 plugin for authentication and user profile retrieval

--- example/providersPlugin.ts
+++ example/providersPlugin.ts
@@ -5,11 +5,13 @@ import { anilistPlugin } from './providers/anilist';
 import { atlassianPlugin } from './providers/atlassian';
 import { facebookPlugin } from './providers/facebook';
 import { googlePlugin } from './providers/google';
+import { applePlugin } from './providers/apple';
 
 export const providersPlugin = new Elysia()
 	.use(fortyTwoPlugin)
 	.use(amazonCognitoPlugin)
 	.use(anilistPlugin)
+	.use(applePlugin)
 	.use(atlassianPlugin)
 	.use(googlePlugin)
 	.use(facebookPlugin);
